32aae432ebebb49204a06710ca2d69db480ee17b
Rafael Almeida da Silva <rafael.silva_Pessoal@outlook.com>
2019-03-22 22:03:44 -0300

    add selects with like , in , not in

--- plsqlAula15-03.sql
+++ plsqlAula15-03.sql
@@ -1,10 +1,10 @@
 
  /*uso de operacao com alias */
 select 
-first_name NOME, last_name SOBRENOME, salary SALÁRIO, salary*(1+ commission_pct) "SALÁRIO COM COMISSÃO",  (salary*(1+ commission_pct))*12 "SALÁRIO ANUAL COM COMISSÃO"
+first_name NOME, last_name SOBRENOME, salary SALÃRIO, salary*(1+ commission_pct) "SALÃRIO COM COMISSÃƒO",  (salary*(1+ commission_pct))*12 "SALÃRIO ANUAL COM COMISSÃƒO"
 from employees;
 
- /*uso de concatenação*/
+ /*uso de concatenaÃ§Ã£o*/
 select 
 first_name|| ' ' || last_name "Nome Completo"
 from employees;
@@ -45,3 +45,35 @@ select
   from employees
   
   where commission_pct >0;
+                                                                                                                   
+                                                                                                                   select FIRST_NAME || ' ' || LAST_NAME "Nome completo"
+from employees
+where hire_date between '01/01/2006' and '31/12/2006';
+
+----------------------------------------------------------------------
+
+select * --FIRST_NAME || ' ' || LAST_NAME "Nome completo"
+from employees
+where commission_pct not in (0.1, 0.3, 0.2, 0.25);
+
+-----------------------------------------------------------------------
+
+select FIRST_NAME
+from employees
+where first_name like '%a%'; /*busca qualquer padrao a no campo fist_name*/
+
+------------------------------------------------------------------------
+select FIRST_NAME
+from employees
+where first_name like '_a_'; /*busca qualquer padrao a no campo fist_name*/
+
+--------------------------------------------------------------------------
+select FIRST_NAME || ' ' || LAST_NAME 
+from employees
+order by first_name
+
+--------------------------------------------------------------
+
+select FIRST_NAME || ' ' || LAST_NAME as nome
+from employees
+order by nome
