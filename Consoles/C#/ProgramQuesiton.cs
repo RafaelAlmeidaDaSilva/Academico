7318f13b0b4d701ab97105967331851124fc6fdd
Rafael Almeida da Silva <rafael.silva_Pessoal@outlook.com>
2019-03-26 17:59:28 -0300

    Represetation of the quesitons on the code C#

---
+++ Consoles/C#/ProgramQuesiton.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+	}
+}
+
+public class Answer {}
+
+public class AnswerTrueFalse : Answer
+{
+	public bool ans {get;set;}
+}
+
+public class AnswerAlternative: Answer
+{
+	public char ans {get;set;}
+}
+
+public class Question
+{ 
+	public string ask {get;set;}
+	public Answer ansSolution {get;set;}
+	public Answer ansResponse {get;set;}
+	public bool right{ get;set;}
+	
+	public void   correct()
+	{
+		// verificar o tipo de resposta essa question possui
+		 Answer s = new AnswerAlternative();
+		 Answer a = new AnswerTrueFalse();
+		Type tTrueFalse = a.GetType();
+		Type tAlternative = s.GetType();
+		
+    
+		 
+		Type t = ansSolution.GetType();
+    
+		if(t.Equals(s.GetType())) // type alternative
+		{
+				// verificar se os dois objetos são o mesmo tipo de resposta 
+				// comparar adequadamente para cada tipo de objeto
+				if (((AnswerAlternative)ansSolution).ans.Equals(((AnswerAlternative)ansResponse).ans))
+					this.right = true;
+				
+					
+		}
+		if(t.Equals(a.GetType())) // type true or false
+		{
+				// verificar se os dois objetos são o mesmo tipo de resposta 
+				// comparar adequadamente para cada tipo de objeto 
+				if (((AnswerTrueFalse)ansSolution).ans.Equals(((AnswerTrueFalse)ansResponse).ans))
+					this.right = true;
+		}
+		
+	
+	
+	}
+	
+
+}
+
+
+public class Quiz 
+{
+	public List<Question> questions {get;set;}
+	
+	public correctQuestions(){
+		foreach(Question q in questions)
+		{
+			q.correct();
+		}
+	}
+	
+}
